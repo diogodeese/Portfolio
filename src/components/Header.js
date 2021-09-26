65a033067105ccfdb65caea67dfdaa777849d175
Diogo Santos <diogodeese@gmail.com>
2021-09-26 22:38:11 +0100

    First Commit

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../App.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-name">
+        <h1>Diogo Santos</h1>
+      </div>
+      <div className="header-links">
+        <p>asd</p>
+        <p>asdafasfd</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
