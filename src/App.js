e82226a7d90774324e87f59f2c58fa08db92d4c4
Diogo Santos <diogodeese@gmail.com>
2021-09-29 18:48:36 +0100

    Added New Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./App.css";
+
 import Navbar from "./components/Navbar/Navbar";
 import Presentation from "./components/Presentation/Presentation";
 import Projects from "./components/Projects/Projects";
+import Skillset from "./components/Skillset/Skillset";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
@@ -10,6 +13,8 @@ function App() {
       <Navbar />
       <Presentation />
       <Projects />
+      <Skillset />
+      <Contact />
     </div>
   );
 }
