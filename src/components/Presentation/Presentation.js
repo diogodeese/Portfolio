fb0e178f573b42968f5f9b737db7a43c0423b064
Diogo Santos <diogodeese@gmail.com>
2021-09-29 02:37:17 +0100

    Updated Presentation

--- src/components/Presentation/Presentation.js
+++ src/components/Presentation/Presentation.js
@@ -1,24 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
+import useWindowDimensions from "../WindowDimensions";
 import "./Presentation.css";
 import img from "../../assets/image.jpg";
 
-class Presentation extends Component {
-  render() {
-    return (
-      <div className="Presentation">
-        <div className="Presentation-leftside">
-          <h1 className="Presentation-title">I like {"\n"} to Code</h1>
-        </div>
-        <div className="Presentation-rightside">
-          <img
-            src={img}
-            alt="My Beautiful Face"
-            style={{ width: 640, height: 360, margin: 100 }}
-          />
-        </div>
-      </div>
-    );
-  }
-}
+export default function Presentation() {
+  const { height } = useWindowDimensions();
 
-export default Presentation;
+  return (
+    <div id="home" className="Presentation" style={{ height: height }}>
+      <div className="Presentation-leftside">
+        <h1 className="Presentation-title">I like {"\n"} to Code</h1>
+      </div>
+      <div className="Presentation-rightside">
+        <img
+          src={img}
+          alt="My Beautiful Face"
+          style={{ width: 640, height: 360, margin: 100, borderRadius: 10 }}
+        />
+      </div>
+    </div>
+  );
+}
