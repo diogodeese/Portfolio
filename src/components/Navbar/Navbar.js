3ca18c48475236f373839b24b21d3cec0b553c81
Diogo Santos <diogodeese@gmail.com>
2021-09-29 02:37:26 +0100

    Updated Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Pages } from "./Pages";
+import { Link } from "react-scroll";
+//import { Pages } from "./Pages";
 import "./Navbar.css";
 
 class Navbar extends Component {
@@ -7,17 +8,52 @@ class Navbar extends Component {
     return (
       <nav className="Navbar">
         <h1 className="Navbar-title">Diogo Santos</h1>
-        <ul className="Navbar-menu">
-          {Pages.map((page, key) => {
-            return (
-              <li key={key}>
-                <a className="Navbar-links" href={page.url}>
-                  {page.title}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
+        <div className="Navbar-menu">
+          <Link
+            activeClass="active"
+            to="home"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={1000}
+            className="Navbar-links"
+          >
+            Home
+          </Link>
+          <Link
+            activeClass="active"
+            to="projects"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={1000}
+            className="Navbar-links"
+          >
+            Projects
+          </Link>
+          <Link
+            activeClass="active"
+            to="skillset"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={1000}
+            className="Navbar-links"
+          >
+            Skillset
+          </Link>
+          <Link
+            activeClass="active"
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={1000}
+            className="Navbar-links"
+          >
+            Contact
+          </Link>
+        </div>
       </nav>
     );
   }
