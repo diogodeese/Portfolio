1be243203757f6f580e5c87bf43a9958d51cd0d6
Diogo Santos <diogodeese@gmail.com>
2021-09-29 02:38:03 +0100

    Added Projects

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Presentation from "./components/Presentation/Presentation";
+import Projects from "./components/Projects/Projects";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Presentation />
+      <Projects />
     </div>
   );
 }
