07470e663fa1b9e32336961fe927bc7f7e19348f
Diogo Santos <diogodeese@gmail.com>
2021-09-30 02:31:56 +0100

    Navbar Disappears When Scolling Down

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,45 @@
 import React, { Component } from "react";
 import { Link } from "react-scroll";
-//import { Pages } from "./Pages";
 import "./Navbar.css";
 
 class Navbar extends Component {
+  state = {
+    auth: false,
+    slide: 0, // How much should the Navbar slide up or down
+    lastScrollY: 0, // Keep track of current position in state
+  };
+
+  componentWillMount() {
+    // When this component mounts, begin listening for scroll changes
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    // If this component is unmounted, stop listening
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll = () => {
+    const { lastScrollY } = this.state;
+    const currentScrollY = window.scrollY;
+
+    if (currentScrollY > lastScrollY) {
+      this.setState({ slide: "-80px" });
+    } else {
+      this.setState({ slide: "0px" });
+    }
+    this.setState({ lastScrollY: currentScrollY });
+  };
+
   render() {
     return (
-      <nav className="Navbar">
+      <nav
+        className="Navbar"
+        style={{
+          transform: `translate(0, ${this.state.slide})`,
+          transition: "transform 300ms linear",
+        }}
+      >
         <h1 className="Navbar-title">Diogo Santos</h1>
         <div className="Navbar-menu">
           <Link
