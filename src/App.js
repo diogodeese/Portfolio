652c980a65a2fadad544cb1469e5764ad39339ef
Diogo Santos <diogodeese@gmail.com>
2021-10-14 02:31:56 +0100

    Added Sidebars

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./App.css";
 
 import Navbar from "./components/Navbar/Navbar";
+import LeftSidebar from "./components/Sidebar/LeftSidebar";
+import RightSidebar from "./components/Sidebar/RightSidebar";
 import Presentation from "./components/Presentation/Presentation";
 import Projects from "./components/Projects/Projects";
 import Skillset from "./components/Skillset/Skillset";
@@ -11,6 +13,8 @@ function App() {
   return (
     <div>
       <Navbar />
+      <LeftSidebar />
+      <RightSidebar />
       <Presentation />
       <Projects />
       <Skillset />
