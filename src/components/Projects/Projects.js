84cf4077c28c950d26acf6273bf4a9b4fca38728
Diogo Santos <diogodeese@gmail.com>
2021-10-15 02:19:33 +0100

    Added Projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,24 +1,52 @@
 import React from "react";
 import useWindowDimensions from "../WindowDimensions";
+import { FiGithub } from "react-icons/fi";
 import "./Projects.css";
-import img from "../../assets/image.jpg";
 
 export default function Projects() {
-  const { height } = useWindowDimensions();
+  const { height, width } = useWindowDimensions();
 
   return (
     <div id="projects" className="Projects" style={{ height: height }}>
-      <div className="Project">
-        <div className="Project-title">
-          <h3>Hi, my name is</h3>
-        </div>
-        <div className="Project-image">
-          <img
-            src={img}
-            alt="Project"
-            style={{ width: 640, height: 360, borderRadius: 5 }}
-          />
-        </div>
+      <div className="Projects-introduction">
+        <h1 className="Projects-title">What have I built</h1>
+      </div>
+      <div className="Projects-container">
+        <table>
+          <tr>
+            <td>
+              <div className="Project">
+                <h2>Mobile Zoo App</h2>
+                <h4>
+                  I build this mobile application because when I visited a zoo I
+                  couldn't remember wich animals i did see.
+                </h4>
+                <div className="Project-footer">
+                  <ul>
+                    <li>React Native</li>
+                    <li>JS</li>
+                    <li>{width}</li>
+                  </ul>
+                  <FiGithub className="Project-footer-github" />
+                </div>
+              </div>
+            </td>
+            <td>
+              <div className="Project"></div>
+            </td>
+            <td>
+              <div className="Project"></div>
+            </td>
+            <td>
+              <div className="Project"></div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className="Project"></div>
+            </td>
+          </tr>
+        </table>
       </div>
     </div>
   );
