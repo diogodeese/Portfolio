e059017c634ce27b5a2da16020665ba358f89601
Diogo Santos <diogodeese@gmail.com>
2021-10-16 03:45:28 +0100

    Created a Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div id="footer" style={{ height: 200 }}>
+      <h2 style={{ textAlign: "center", color: "white" }}>
+        Built by Diogo Santos
+      </h2>
+    </div>
+  );
+}
