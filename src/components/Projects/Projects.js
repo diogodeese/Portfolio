4b454a6000246c35e9f17470347d49c1a8552308
Diogo Santos <diogodeese@gmail.com>
2021-10-18 02:51:52 +0100

    Changed Projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,22 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useWindowDimensions from "../WindowDimensions";
 import { FiFolder, FiGithub, FiExternalLink } from "react-icons/fi";
-import { projects } from "./DoneProjects";
+import { projects } from "./MyProjects";
 import "./Projects.css";
 
 export default function Projects() {
-  const { height } = useWindowDimensions();
+  const { height, width } = useWindowDimensions();
+  const [projectsNumber, setProjectsNumber] = useState(8);
+  const [adicionalHeigh, setAdicionalHeigh] = useState(0);
+
+  useEffect(() => {
+    if (width < 1600) {
+      setProjectsNumber(6);
+    }
+
+    if (width < 1200) {
+      setProjectsNumber(4);
+    }
+
+    if (width < 900) {
+      setProjectsNumber(3);
+      setAdicionalHeigh(height * 2);
+    }
+  }, [width, height]);
 
   return (
-    <div id="projects" className="Projects" style={{ height: height }}>
+    <div
+      id="projects"
+      className="Projects"
+      style={{ height: height + adicionalHeigh }}
+    >
       <div className="Projects-introduction">
         <h1 className="Projects-title">What have I built</h1>
       </div>
       <div className="Projects-container">
         <div className="Project-wrapper">
-          {projects.map((project) => {
+          {projects.slice(0, projectsNumber).map((project, index) => {
             return (
-              <div className="Project">
+              <div key={index} className="Project">
                 <FiFolder className="Project-main-icon" />
                 <ul className="Project-links">
                   {project.links
