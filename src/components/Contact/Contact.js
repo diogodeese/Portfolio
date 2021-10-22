368e3b7038991fa89968c2ab737bef29a8b6d921
Diogo Santos <diogodeese@gmail.com>
2021-10-22 04:30:38 +0100

    Created Contact Section

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,5 +5,21 @@ import "./Contact.css";
 export default function Contact() {
   const { height } = useWindowDimensions();
 
-  return <div id="contact" style={{ height: height }}></div>;
+  return (
+    <div id="contact" className="Contact" style={{ height: height }}>
+      <div className="Contact-container">
+        <h2 className="Contact-title">Get In Touch</h2>
+        <h4 className="Contact-description">
+          Currently looking for a job and open to create interesting projects,
+          if you have something in mind that you would like to share with me
+          contact me!
+        </h4>
+        <div className="Contact-button-div">
+          <a className="Contact-button" href="mailto:diogodeese@gmail.com">
+            Hello World
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
