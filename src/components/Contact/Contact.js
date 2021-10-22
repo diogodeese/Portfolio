076b670b74412eaf0b2597743e84063dd21e5599
Diogo Santos <diogodeese@gmail.com>
2021-10-23 00:26:00 +0100

    Contact Corrections

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -10,13 +10,13 @@ export default function Contact() {
       <div className="Contact-container">
         <h2 className="Contact-title">Get In Touch</h2>
         <h4 className="Contact-description">
-          Currently looking for a job and open to create interesting projects,
+          Currently looking for a job and open to develop interesting projects,
           if you have something in mind that you would like to share with me
           contact me!
         </h4>
         <div className="Contact-button-div">
           <a className="Contact-button" href="mailto:diogodeese@gmail.com">
-            Hello World
+            Say Hello
           </a>
         </div>
       </div>
