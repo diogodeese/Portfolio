96752e357a97e2038a088cd914085e7fd3a01909
Diogo Santos <diogodeese@gmail.com>
2021-10-23 05:46:42 +0100

    Removed Navbar

--- src/pages/Archive.js
+++ src/pages/Archive.js
@@ -1,14 +1,14 @@
-import Navbar from "../components/Navbar/Navbar";
 import LeftSidebar from "../components/Sidebar/LeftSidebar";
 import RightSidebar from "../components/Sidebar/RightSidebar";
+import ProjectsArchive from "../components/ProjectsArchive/ProjectsArchive";
 import Footer from "../components/Footer/Footer";
 
 export default function Archive() {
   return (
     <div>
-      <Navbar />
       <LeftSidebar />
       <RightSidebar />
+      <ProjectsArchive />
       <Footer />
     </div>
   );
