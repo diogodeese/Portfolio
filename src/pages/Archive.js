a6b35bb4b4d1cf8e6bb19b2f160837e8e47e642d
Diogo Santos <diogodeese@gmail.com>
2021-10-23 20:02:51 +0100

    Added Navbar to the Archive

--- src/pages/Archive.js
+++ src/pages/Archive.js
@@ -1,3 +1,4 @@
+import NavbarArchive from "../components/Navbar/NavbarArchive";
 import LeftSidebar from "../components/Sidebar/LeftSidebar";
 import RightSidebar from "../components/Sidebar/RightSidebar";
 import ProjectsArchive from "../components/ProjectsArchive/ProjectsArchive";
@@ -6,6 +7,7 @@ import Footer from "../components/Footer/Footer";
 export default function Archive() {
   return (
     <div>
+      <NavbarArchive />
       <LeftSidebar />
       <RightSidebar />
       <ProjectsArchive />
