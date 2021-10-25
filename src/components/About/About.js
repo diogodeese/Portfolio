e93ad7017fc0690bf1b6049f97e4507a501d7e81
Diogo Santos <diogodeese@gmail.com>
2021-10-25 04:39:03 +0100

    Changed About

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,5 +5,30 @@ import "./About.css";
 export default function About() {
   const { height } = useWindowDimensions();
 
-  return <div id="about" style={{ height: height }}></div>;
+  return (
+    <div id="about" className="About" style={{ height: height }}>
+      <div className="About-left-container">
+        <div style={{ display: "block" }}>
+          <h1 className="About-title">About Me</h1>
+          <h3 className="About-description">
+            Im a really cool person that have no idea what to write in here, so
+            Im just going to write non sense things and hope that someone helps
+            me to figure out what Im.
+            <br /> <br />
+            Im probably a good person my Im not that sure, but I hope Im.
+            <br /> <br />
+            Faith in god.
+          </h3>
+          <ul>
+            <li>React</li>
+            <li>JavaScript</li>
+            <li>Node.js</li>
+          </ul>
+        </div>
+      </div>
+      <div className="About-right-container">
+        <h1>Image</h1>
+      </div>
+    </div>
+  );
 }
