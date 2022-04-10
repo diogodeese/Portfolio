a69dc65ab2a01a29d0bae8f7813deefb0eed9fbd
Diogo Santos <diogodeese@gmail.com>
2022-04-10 21:59:55 +0100

    Navbar href update

--- src/components/Navbar/NavbarArchive.js
+++ src/components/Navbar/NavbarArchive.js
@@ -1,22 +1,16 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
+
 import "./Navbar.css";
 
-export default function NavbarArchive() {
-  const history = useHistory();
+// Styled Components
+import { AButton } from "../styles/Button.styled";
 
+export default function NavbarArchive() {
   return (
     <nav className="Navbar">
       <h1 className="NavbarArchive-title">Diogo Santos</h1>
       <div className="Navbar-menu">
-        <button
-          className="Navbar-button"
-          onClick={() => {
-            history.push("/");
-          }}
-        >
-          Go Back
-        </button>
+        <AButton href="/">Go Back</AButton>
       </div>
     </nav>
   );
