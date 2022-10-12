bd31a4ab0927d30c5de33607079c20335bc31282
Diogo Santos <53911973+diogodeese@users.noreply.github.com>
2022-10-12 04:42:36 +0100

    style: list bullet points

--- src/components/styles/Lists.styled.js
+++ src/components/styles/Lists.styled.js
@@ -19,6 +19,15 @@ export const Li = styled.li`
   margin-right: 100px;
   width: fit-content;
 
+  &:before {
+    content: '▹';
+    position: absolute;
+    left: 0;
+    color: ${({theme}) => theme.colors.valencia};
+    font-size: 12px;
+    line-height: 12px;
+  }
+
   @media only screen and (max-width: ${({ theme }) => theme.breakpoints.sm}) {
     font-size: 12px;
   }
