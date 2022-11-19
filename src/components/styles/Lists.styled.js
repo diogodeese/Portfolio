a4e4343df313a3d10d6e16dc0abc128c40f2f018
Diogo Santos <diogodeese@gmail.com>
2022-11-19 23:15:53 +0000

    style: list bullet point

--- src/components/styles/Lists.styled.js
+++ src/components/styles/Lists.styled.js
@@ -2,22 +2,23 @@ import styled from "styled-components";
 
 export const Ul = styled.ul`
   margin-bottom: 5px;
-  font-family: "SF Mono", "Fira Code", "Fira Mono", "Roboto Mono", monospace;
-  font-size: 16px;
   color: ${({ theme }) => theme.colors.grey};
-
-  @media only screen and (max-width: ${({ theme }) => theme.breakpoints.sm}) {
-    font-size: 12px;
-  }
 `;
 
 export const Li = styled.li`
   margin-bottom: 5px;
-  font-family: "SF Mono", "Fira Code", "Fira Mono", "Roboto Mono", monospace;
-  font-size: 16px;
+  font-family: "Rubik", "SF Mono", "Fira Code", "Fira Mono", "Roboto Mono",
+    monospace;
+  font-size: 14px;
   color: ${({ theme }) => theme.colors.grey};
   margin-right: 100px;
   width: fit-content;
+
+  &::before {
+    content: "▹";
+    margin-right: 5px;
+    color: ${({ theme }) => theme.colors.valencia};
+  }
 
   @media only screen and (max-width: ${({ theme }) => theme.breakpoints.sm}) {
     font-size: 12px;
